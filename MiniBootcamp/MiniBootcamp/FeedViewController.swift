70ae6a3d96d12d689e07f1b631fdcea27b53f396
ferdelarosa-wz <fernando.delarosa@wizeline.com>
2023-05-10 18:39:57 -0600

    Create a table view for FeedViewController

--- MiniBootcamp/MiniBootcamp/FeedViewController.swift
+++ MiniBootcamp/MiniBootcamp/FeedViewController.swift
@@ -9,8 +9,40 @@ import UIKit
 
 final class FeedViewController: UIViewController {
     
+    private(set) lazy var tableView: UITableView = {
+       let tableView =  UITableView()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         title = "TweetFeed"
         view.backgroundColor = .white
+        setupTableView()
+    }
+    
+    private func setupTableView() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ])
+    }
+}
+
+extension FeedViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        .zero
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
     }
 }
--- MiniBootcamp/MiniBootcampTests/FeedViewControllerTests.swift
+++ MiniBootcamp/MiniBootcampTests/FeedViewControllerTests.swift
@@ -18,4 +18,23 @@ final class FeedViewControllerTests: XCTestCase {
         XCTAssertEqual(sut.title, "TweetFeed")
         XCTAssertEqual(sut.view.backgroundColor, UIColor.white)
     }
+    
+    func test_createATableView() throws {
+        let sut = FeedViewController()
+        
+        sut.loadViewIfNeeded()
+        
+        let tableView = sut.tableView
+        let viewContainsTableView = try XCTUnwrap(sut.view?.subviews.contains(tableView))
+        XCTAssert(viewContainsTableView)
+    }
+    
+    func test_setupTableView_delegates() {
+        let sut = FeedViewController()
+        
+        sut.loadViewIfNeeded()
+        
+        XCTAssert(sut.tableView.delegate is FeedViewController)
+        XCTAssert(sut.tableView.dataSource is FeedViewController)
+    }
 }
